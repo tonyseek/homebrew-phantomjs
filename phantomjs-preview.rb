bc2e134d5a4a318de3e8d8680b8c3ddebad21c4b
Jiangge Zhang <tonyseek@gmail.com>
2015-11-11 14:43:31 +0800

    Add the preview release formula.
    Links:
     - https://github.com/Homebrew/homebrew/issues/42249#issuecomment-151422574
     - https://github.com/Vitallium/phantomjs/releases/tag/2.0.1

---
+++ phantomjs-preview.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'phantomjs' do
+  version '2.0.1'
+  sha256 '0c3613d21187b4f3d02d3df9597800f404ff872bd43b748fb3fad84da425ae85'
+
+  url "https://github.com/Vitallium/phantomjs/releases/download/#{version}/phantomjs-#{version}-macosx.zip"
+  name 'PhantomJS'
+  homepage 'http://phantomjs.org/'
+  license :bsd
+
+  binary "phantomjs-#{version}-macosx/bin/phantomjs"
+end
